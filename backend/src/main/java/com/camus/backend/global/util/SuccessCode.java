1e0c71c30e68d63421dc5e2d13a7e56aa167b726
damdam6 <dambi626626@gmail.com>
2024-04-25 17:30:52 +0900

    feat: Success 동일값에 대한 응답 util
    - SuccessCode 리스트업
    - SuccessCode 기반 SuccessResponseDto 구현
    
    Related to: #S10P31A603-138

---
+++ backend/src/main/java/com/camus/backend/global/util/SuccessCode.java
@@ -0,0 +1,28 @@
+package com.camus.backend.global.util;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum SuccessCode {
+
+	// MEMBER
+	LOGOUT("MEMBER", "로그아웃 성공"),
+	PROFILE_EDIT("MEMBER", "프로필 사진 변경 완료"),
+	NICKNAME_EDIT("MEMBER", "닉네임 변경 완료"),
+
+	// CHANEL
+	CHANNEL_DISABLE("CHANEL", "채널 링크 비활성화 성공"),
+	CHANEL_EDIT("CHANEL", "채널명 변경 성공"),
+
+	// ROOM
+	ROOM_DISABLE("ROOM", "채팅방 닫기 성공"),
+	
+	;
+
+	private final String okResponse = "OK";
+	private final String feature;
+	private final String successMessage;
+
+}
