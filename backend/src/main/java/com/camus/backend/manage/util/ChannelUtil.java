d2419279f5c656746b7cc59ae3d47124efb2b654
damdam6 <dambi626626@gmail.com>
2024-04-30 15:38:59 +0900

    feat: ChannelUtil 에러처리 구현
    - title 사이즈 에러 처리 구현
    - content 사이즈 에러처리 구현
    - filterLevel 범위 에러처리 구현
    - type 에러 처리 구현
    
    Related to: #S10P31A603-141

---
+++ backend/src/main/java/com/camus/backend/manage/util/ChannelUtil.java
@@ -0,0 +1,39 @@
+package com.camus.backend.manage.util;
+
+import com.camus.backend.global.Exception.CustomException;
+import com.camus.backend.global.Exception.ErrorCode;
+
+public class ChannelUtil {
+
+	private ChannelUtil() {
+
+	}
+
+	public static void checkChannelTitleLengthLimit(String title) {
+		if (title.length() > ManageConstants.MAX_CHANNEL_TITLE_LENGTH) {
+			throw new CustomException(ErrorCode.INVALID_PARAMETER_CHANNEL_TITLE);
+		}
+		if (title.length() < ManageConstants.MIN_CHANNEL_TITLE_LENGTH) {
+			throw new CustomException(ErrorCode.INVALID_PARAMETER_CHANNEL_TITLE);
+		}
+	}
+
+	public static void checkChannelContentLengthLimit(String content) {
+		if (content.length() > ManageConstants.MAX_CHANNEL_CONTENTS_LENGTH) {
+			throw new CustomException(ErrorCode.INVALID_PARAMETER_CHANNEL_CONTENT);
+		}
+	}
+
+	public static void checkChannelFilterLevel(int filterLevel) {
+		if (filterLevel < ManageConstants.CHANNEL_FILTER_MIN_LEVEL
+			|| filterLevel > ManageConstants.CHANNEL_FILTER_MAX_LEVEL) {
+			throw new CustomException(ErrorCode.INVALID_PARAMETER_CHANNEL_FILTER_LEVEL);
+		}
+	}
+
+	public static void checkChannelType(String type) {
+		if (!type.equals(ManageConstants.CHANNEL_TYPE_PRIVATE) && !type.equals(ManageConstants.CHANNEL_TYPE_GROUP)) {
+			throw new CustomException(ErrorCode.INVALID_PARAMETER_CHANNEL_TYPE);
+		}
+	}
+}
