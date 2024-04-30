a4b997ebbb1fd38c9361f7a905380e8de1c91b95
damdam6 <dambi626626@gmail.com>
2024-04-30 15:36:24 +0900

    feat: RequestBody Error 핸들러 추가
    - ErrorCode enum 추가 (공통 항목)
    - requestBody 오류 handler 추가
    
    Related to: #S10P31A603-184

--- backend/src/main/java/com/camus/backend/global/Exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/camus/backend/global/Exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,5 +17,15 @@ public class GlobalExceptionHandler {
 		body.put("errorKey", ex.getErrorKey());
 		body.put("message", ex.getMessage());
 		return new ResponseEntity<>(body, ex.getHttpStatusCode());
+	}
+
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<?> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+		Map<String, Object> body = new LinkedHashMap<>();
+		body.put("status", ErrorCode.INVALID_PARAMETER.getHttpStatusCode());
+		body.put("errorKey", ErrorCode.INVALID_PARAMETER.getErrorKey());
+		body.put("message", ErrorCode.INVALID_PARAMETER.getErrorMessage());
+
+		return new ResponseEntity<>(body, ErrorCode.INVALID_PARAMETER.getHttpStatusCode());
 	}
 }
