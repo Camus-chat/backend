91eb19a02a941c80971e3d341943cb59f133571b
damdam6 <dambi626626@gmail.com>
2024-05-13 12:07:17 +0900

    fix : 주소 변경
    Related to: #S10P31A603-215

--- backend/src/main/java/com/camus/backend/global/config/SwaggerConfig.java
+++ backend/src/main/java/com/camus/backend/global/config/SwaggerConfig.java
@@ -6,12 +6,16 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import lombok.RequiredArgsConstructor;
 
 @OpenAPIDefinition(
 	info = @Info(title = "CAMUS",
 		description = "CAMUS api document",
-		version = "v1"))
+		version = "v1"),
+	servers = @Server(url = "http://localhost:8080", description = "test server")
+)
+
 @RequiredArgsConstructor
 @Configuration
 public class SwaggerConfig {
