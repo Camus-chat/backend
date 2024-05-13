1b563c8bb94318d6a95cf1e06f81d3789625fa7d
damdam6 <dambi626626@gmail.com>
2024-05-13 12:04:42 +0900

    feat: chatMessage MaxSize 설정
    Related to: #S10P31A603-215

--- backend/src/main/java/com/camus/backend/chat/util/ChatConstants.java
+++ backend/src/main/java/com/camus/backend/chat/util/ChatConstants.java
@@ -1,6 +1,11 @@
 package com.camus.backend.chat.util;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public final class ChatConstants {
+	public int CHAT_MESSAGE_PAGE_SIZE = 300;
+
 	public String REDIS_STREAM_COUNTER_KEY(String roomId) {
 		return "chat:room:" + roomId + ":counter";
 	}
