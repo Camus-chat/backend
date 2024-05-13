0eeed9acb152004160e916185d977e243a7194ab
damdam6 <dambi626626@gmail.com>
2024-05-13 12:07:38 +0900

    test : TestController 구현
    - api 테스트용 Controller
    
    Related to: #S10P31A603-215

---
+++ backend/src/main/java/com/camus/backend/chat/controller/TestController.java
@@ -0,0 +1,36 @@
+package com.camus.backend.chat.controller;
+
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.camus.backend.chat.service.RedisChatService;
+import com.camus.backend.manage.util.ManageConstants;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+	private final RedisChatService redisChatService;
+
+	private TestController(RedisChatService redisChatService) {
+		this.redisChatService = redisChatService;
+	}
+
+	@PostMapping("/redisCreateRoomNoticeTest")
+	public ResponseEntity<String> redisTest(
+		@RequestBody UUID roomId
+	) {
+
+		UUID tempMemberId = ManageConstants.tempMemUuid;
+		redisChatService.createChatRoomNotice(
+			roomId.toString(),
+			tempMemberId
+		);
+		return ResponseEntity.ok("ok");
+	}
+}
