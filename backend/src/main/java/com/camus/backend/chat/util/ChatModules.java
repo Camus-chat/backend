2d071602a90f9d0a76b3e23144dd66599489efb8
damdam6 <dambi626626@gmail.com>
2024-05-13 12:08:59 +0900

    feat: ChatModules 유틸 구현
    - Key별 String 생성 이름
    - DB startPage Index 구하는 유틸 구현
    
    Related to: #S10P31A603-215

--- backend/src/main/java/com/camus/backend/chat/util/ChatModules.java
+++ backend/src/main/java/com/camus/backend/chat/util/ChatModules.java
@@ -7,4 +7,23 @@ public class ChatModules {
 	public String getRedisCountKey(String roomId) {
 		return "chat:room:" + roomId + ":counter";
 	}
+
+	public String getRedisStreamKey(String roomId) {
+		return "chat:room:" + roomId + ":stream";
+	}
+
+	public String getRedisSavedLastMessageKey(String roomId) {
+		return "chat:room:" + roomId + ":lastMessage";
+	}
+
+	public String getStreamConsumerGroupKey(String roomId) {
+		return "chat:room:" + roomId + ":group";
+	}
+
+	public long getMogoDBStartPageIndex(
+		long lastMessageId,
+		long streamMessageCount
+	) {
+		return (lastMessageId - streamMessageCount) / 300;
+	}
 }
