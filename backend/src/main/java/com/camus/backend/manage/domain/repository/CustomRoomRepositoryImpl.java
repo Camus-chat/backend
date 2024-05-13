5e0b573d0a9ec75632215d0a0ecb5cc740da194b
damdam6 <dambi626626@gmail.com>
2024-05-13 12:10:35 +0900

    feat: ChatData Repository 구현
    - ChatData Repository 구현
    - RedisGroupRespository 구현
    - MongoRepository 구현
    - RedisChatRepository 구현
    
    Related to: #S10P31A603-215

--- backend/src/main/java/com/camus/backend/manage/domain/repository/CustomRoomRepositoryImpl.java
+++ backend/src/main/java/com/camus/backend/manage/domain/repository/CustomRoomRepositoryImpl.java
@@ -8,12 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
 
 import com.camus.backend.manage.domain.document.Channel;
 import com.camus.backend.manage.domain.document.Room;
 import com.camus.backend.manage.domain.dto.RoomDto;
 import com.camus.backend.manage.util.ChannelStatus;
 
+@Repository
 public class CustomRoomRepositoryImpl implements CustomRoomRepository {
 
 	@Autowired
