23fd72cfa369eb5bbfbe1d995e447bdc5172c134
damdam6 <dambi626626@gmail.com>
2024-05-14 09:57:39 +0900

    feat: ChatDataDto 수정
    - nextRedisMessageId로 수정
    
    Related to: #S10P31A603-215

--- backend/src/main/java/com/camus/backend/chat/domain/dto/PaginationDto.java
+++ backend/src/main/java/com/camus/backend/chat/domain/dto/PaginationDto.java
@@ -1,15 +1,15 @@
 package com.camus.backend.chat.domain.dto;
 
-import java.util.UUID;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
-public class ChatDataRequestDto {
-	UUID roomId;
-	String nextMessageTimeStamp = "0-0";
+@RequiredArgsConstructor
+public class PaginationDto {
+	private String nextMessageTimeStamp;
+	private int size;
 }
