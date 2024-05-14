1a2d3161f4720b6600b1d4d0e8a8511d6cf06ebe
dtdtdz <dtdtdz@gmail.com>
2024-05-15 02:14:08 +0900

    feat: filteredType 설정
    - filteredType을 각 label과 level에 따라 분류
    
    Related to: #S10P31A603-198

---
+++ backend/src/main/java/com/camus/backend/filter/util/type/FilteredType.java
@@ -0,0 +1,16 @@
+package com.camus.backend.filter.util.type;
+
+public enum FilteredType {
+	MALICIOUS_SIMPLE,
+	MALICIOUS_LAMBDA,
+	MALICIOUS_CLOVA,
+	HATE_LAMBDA,
+	HATE_CLOVA,
+	SPAM,
+	NOT_FILTERED
+
+
+//String type으로 변환 필요?
+//더 clova, lambda 분류해야하나?
+	
+}
