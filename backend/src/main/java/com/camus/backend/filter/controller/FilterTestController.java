e883c3c2437eadecb8b7ca9607fe7ab7151ef5e9
dtdtdz <dtdtdz@gmail.com>
2024-05-15 03:01:38 +0900

    feat: model data parsing 예시
    - test controller에 model data parsing 예시
    
    Related to: #S10P31A603-198

--- backend/src/main/java/com/camus/backend/filter/controller/FilterTestController.java
+++ backend/src/main/java/com/camus/backend/filter/controller/FilterTestController.java
@@ -1,7 +1,9 @@
 package com.camus.backend.filter.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import org.apache.hc.client5.http.async.methods.SimpleHttpResponse;
@@ -12,6 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.camus.backend.chat.domain.document.CommonMessage;
 import com.camus.backend.filter.service.FilterService;
+import com.camus.backend.filter.util.type.FilteredType;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("model")
@@ -19,24 +24,10 @@ public class FilterTestController {
 
 	private final FilterService filterService;
 
-	private final FutureCallback<SimpleHttpResponse> testCallback;
-	public FilterTestController(FilterService clovaService){
+	private final ObjectMapper objectMapper;
+	public FilterTestController(FilterService clovaService, ObjectMapper objectMapper){
 		this.filterService = clovaService;
-		this.testCallback = new FutureCallback<>() {
-			@Override
-			public void completed(SimpleHttpResponse simpleHttpResponse) {
-				System.out.println(simpleHttpResponse.getCode());
-				System.out.println(simpleHttpResponse.getBody().getBodyText());
-			}
-			@Override
-			public void failed(Exception e) {
-				System.out.println(e);
-			}
-			@Override
-			public void cancelled() {
-				System.out.println("cancelled");
-			}
-		};
+		this.objectMapper = objectMapper;
 	}
 	@GetMapping("clova")
 	public void clova(){
@@ -51,8 +42,88 @@ public class FilterTestController {
 			.content("욕설1").build();
 		messages.add(message2);
 
-		filterService.token(messages, testCallback);
-		filterService.predict(messages, testCallback);
+		filterService.token(messages, new FutureCallback<>() {
+			@Override
+			public void completed(SimpleHttpResponse simpleHttpResponse) {
+				if (simpleHttpResponse.getCode()==200){
+					try {
+						Map<?, ?> map = objectMapper.readValue(simpleHttpResponse.getBody().getBodyText(), Map.class);
+						// System.out.println(simpleHttpResponse.getBody().getBodyText());
+						// System.out.println(map);
+						if (((Map<?, ?>)map.get("status")).get("code").equals("20000")){
+							List<?> messages = (List<?>)((Map<?, ?>)map.get("result")).get("messages");
+							int count = (int)((Map<?,?>) messages.get(0)).get("count");
+							System.out.println("count: "+count);
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+						throw new RuntimeException(e);
+					}
+
+				} else throw new RuntimeException("clova token api error");
+			}
+			@Override
+			public void failed(Exception e) {
+				System.out.println(e);
+			}
+			@Override
+			public void cancelled() {
+				System.out.println("clova token cancelled");
+			}
+		});
+		filterService.predict(messages, new FutureCallback<>() {
+			@Override
+			public void completed(SimpleHttpResponse simpleHttpResponse) {
+				if (simpleHttpResponse.getCode()==200){
+					try {
+						Map<?, ?> map = objectMapper.readValue(simpleHttpResponse.getBody().getBodyText(), Map.class);
+						System.out.println(simpleHttpResponse.getBody().getBodyText());
+						System.out.println(map);
+						if (((Map<?, ?>)map.get("status")).get("code").equals("20000")){
+							Map<?,?> message = (Map<?,?>)((Map<?, ?>)map.get("result")).get("message");
+							String[] resultArr = ((String)message.get("content")).split(",");
+							if (resultArr.length != messages.size()) throw new RuntimeException("clova predict failed");
+							String[] resultTypeArr = new String[resultArr.length];
+							for (int i=0; i<resultArr.length; i++){
+								switch (resultArr[i]){
+									case "욕설/비하/모욕":
+										messages.get(i).setFilteredType(FilteredType.MALICIOUS_CLOVA.name());
+										resultTypeArr[i] = FilteredType.MALICIOUS_CLOVA.name();
+										break;
+									case "성희롱/혐오":
+										messages.get(i).setFilteredType(FilteredType.HATE_CLOVA.name());
+										resultTypeArr[i] = FilteredType.HATE_CLOVA.name();
+										break;
+									case "도배/홍보":
+										messages.get(i).setFilteredType(FilteredType.HATE_LAMBDA.name());
+										resultTypeArr[i] = FilteredType.SPAM.name();
+										break;
+									case "중립":
+										messages.get(i).setFilteredType(FilteredType.NOT_FILTERED.name());
+										resultTypeArr[i] = FilteredType.NOT_FILTERED.name();
+										break;
+									default:
+										throw new RuntimeException("clova predict failed");
+								}
+							}
+							System.out.println(Arrays.toString(resultTypeArr));
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+						throw new RuntimeException(e);
+					}
+
+				} else throw new RuntimeException("clova predict api error");
+			}
+			@Override
+			public void failed(Exception e) {
+				System.out.println(e);
+			}
+			@Override
+			public void cancelled() {
+				System.out.println("clova predict cancelled");
+			}
+		});
 	}
 
 	@GetMapping("lambda")
@@ -61,7 +132,42 @@ public class FilterTestController {
 			.senderId(UUID.randomUUID())
 			.content("내용1").build();
 
-		filterService.predict(message, testCallback);
+		filterService.predict(message, new FutureCallback<>() {
+			@Override
+			public void completed(SimpleHttpResponse simpleHttpResponse) {
+				if (simpleHttpResponse.getCode()==200){
+					try {
+						Map<?, ?> map = objectMapper.readValue(simpleHttpResponse.getBody().getBodyText(), Map.class);
+						int value = (int)map.get("prediction");
+						switch (value){
+							case 0:
+								message.setFilteredType(FilteredType.MALICIOUS_LAMBDA.name());
+								break;
+							case 1:
+								message.setFilteredType(FilteredType.HATE_LAMBDA.name());
+								break;
+							case 2:
+								message.setFilteredType(FilteredType.NOT_FILTERED.name());
+								break;
+							default:
+								throw new RuntimeException("lambda predict api error");
+						}
+						System.out.println(message.getFilteredType());
+					} catch (Exception e) {
+						throw new RuntimeException(e);
+					}
+
+				} else throw new RuntimeException("lambda predict api error");
+			}
+			@Override
+			public void failed(Exception e) {
+				System.out.println(e);
+			}
+			@Override
+			public void cancelled() {
+				System.out.println("clova predict cancelled");
+			}
+		});
 	}
 
 	@GetMapping("bad")
@@ -69,6 +175,13 @@ public class FilterTestController {
 		CommonMessage message = CommonMessage.builder()
 			.senderId(UUID.randomUUID())
 			.content("시발").build();
-		System.out.println(filterService.isBadWord(message));
+		if (filterService.isBadWord(message)){
+			message.setFilteredType(FilteredType.MALICIOUS_SIMPLE.name());
+		}
+		else{
+			message.setFilteredType(FilteredType.NOT_FILTERED.name());
+		}
+
+		System.out.println(message.getFilteredType());
 	}
 }
