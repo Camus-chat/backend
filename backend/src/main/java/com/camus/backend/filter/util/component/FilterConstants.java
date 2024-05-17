34d2febe29ccdc74213ed4c4835c229dc19db482
dtdtdz <dtdtdz@gmail.com>
2024-05-17 18:00:40 +0900

    fix: 대문자로 topic, group 수정
    - constants 수정
    
    Related to: #S10P31A603-198

--- backend/src/main/java/com/camus/backend/filter/util/component/FilterConstants.java
+++ backend/src/main/java/com/camus/backend/filter/util/component/FilterConstants.java
@@ -2,11 +2,10 @@ package com.camus.backend.filter.util.component;
 
 import org.springframework.stereotype.Component;
 
-import lombok.Getter;
-
 @Component
 public class FilterConstants {
-	public final String FILTERING_REQ_TOPIC = "filteringRequest";
-	public final String FILTERING_RES_TOPIC = "filteringResponse";
-	public final String FILTERING_GROUP_ID = "filteringGroup";
+	public final String FILTERING_REQ_TOPIC = "FilteringRequest";
+	public final String SINGLE_FILTERING_RES_TOPIC = "SingleFilteringResponse";
+	public final String CONTEXT_FILTERING_RES_TOPIC = "ContextFilteringResponse";
+	public final String FILTERING_GROUP_ID = "FilteringGroup";
 }
