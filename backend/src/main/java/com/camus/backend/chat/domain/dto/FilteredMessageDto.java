92f0cfa537b2617cba9d24ec527aca5fd8ee1869
damdam6 <dambi626626@gmail.com>
2024-05-18 21:56:03 +0900

    fix : parsing오류 수정

--- backend/src/main/java/com/camus/backend/chat/domain/dto/FilteredMessageDto.java
+++ backend/src/main/java/com/camus/backend/chat/domain/dto/FilteredMessageDto.java
@@ -6,10 +6,12 @@ import java.util.UUID;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@NoArgsConstructor
 public class FilteredMessageDto {
 	@JsonProperty("roomId")
 	private UUID roomId;
