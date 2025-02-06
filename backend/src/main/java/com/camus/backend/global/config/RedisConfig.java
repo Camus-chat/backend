770b705e1290f8cb2240d312382c931240d7de55
dtdtdz <69425431+dtdtdz@users.noreply.github.com>
2025-02-06 22:56:10 +0900

    Update RedisConfig.java
    - Set ssl

--- backend/src/main/java/com/camus/backend/global/config/RedisConfig.java
+++ backend/src/main/java/com/camus/backend/global/config/RedisConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
@@ -20,7 +22,26 @@ public class RedisConfig {
 
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory(host, port);
+		RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+		redisConfig.setHostName(host);
+		redisConfig.setPort(port);
+
+		LettuceClientConfiguration clientConfig;
+
+		if (host.equals("localhost"))//local
+		{
+			System.out.println("notSSL");
+			clientConfig = LettuceClientConfiguration
+					.builder()
+					.build();
+		}
+		else {
+			clientConfig = LettuceClientConfiguration
+					.builder()
+					.useSsl()
+					.build();
+		}
+		return new LettuceConnectionFactory(redisConfig, clientConfig);
 	}
 
 	@Bean
