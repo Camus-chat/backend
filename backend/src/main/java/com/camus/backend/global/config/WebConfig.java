3b262fb42bbaf8c7b4f972295156e18bfcad2e02
dtdtdz <69425431+dtdtdz@users.noreply.github.com>
2025-02-12 23:43:02 +0900

    Update WebConfig.java
    - Cors 설정 변경
    - Options 추가

--- backend/src/main/java/com/camus/backend/global/config/WebConfig.java
+++ backend/src/main/java/com/camus/backend/global/config/WebConfig.java
@@ -9,9 +9,11 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry corsRegistry) {
 		corsRegistry.addMapping("/**")
-			.allowedOrigins("*")
-			.allowedMethods("GET", "PUT", "POST", "DELETE", "PATCH");
-
+			.allowedOrigins("http://localhost:3100", "https://camus.life", "https://www.camus.life")
+			.allowedMethods("GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS")
+            .allowedHeaders("*") // 모든 요청 헤더 허용
+			.allowCredentials(true) // 인증정보 포함 (쿠키, Authorization 헤더 등)
+			.maxAge(3600); // CORS 응답을 캐싱하는 시간 (초 단위)
 		//                .allowedOrigins("https://i10a709.p.ssafy.io");
 
 		//        corsRegistry.addMapping("/**")
