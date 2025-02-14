76248c4024cdd5cd877a41332db5939fc9fe18f2
dtdtdz <69425431+dtdtdz@users.noreply.github.com>
2025-02-14 23:47:13 +0900

    Update JwtAuthenticationFilter.java
    - Change accesstoken to cookie

--- backend/src/main/java/com/camus/backend/global/jwt/util/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/camus/backend/global/jwt/util/JwtAuthenticationFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.UUID;
 
+import jakarta.servlet.http.Cookie;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -41,8 +42,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws
 		ServletException, IOException {
 
-		// 헤더에서 access키에 담긴 토큰을 꺼냄
-		String accessToken = request.getHeader("access");
+
+		// 쿠키에서 Access Token 가져오기
+		String accessToken = null;
+		if (request.getCookies() != null) {
+			for (Cookie cookie : request.getCookies()) {
+				if ("accessToken".equals(cookie.getName())) {
+					accessToken = cookie.getValue();
+				}
+			}
+		}
 
 		// 토큰이 없다면 다음 필터로 넘김
 		if (accessToken == null) {
